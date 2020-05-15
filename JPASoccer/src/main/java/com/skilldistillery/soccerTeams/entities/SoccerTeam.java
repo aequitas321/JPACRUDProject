3780606ff81610e5edf90f2d1cd0c3928e2ff2ea
Shane Duncan <aequitas321@icloud.com>
2020-05-15 19:17:36 -0400

    Start JPA project

---
+++ JPASoccer/src/main/java/com/skilldistillery/soccerTeams/entities/SoccerTeam.java
@@ -0,0 +1,56 @@
+package com.skilldistillery.soccerTeams.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "soccer_team")
+public class SoccerTeam {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	public SoccerTeam() {
+		super();
+	}
+
+	public SoccerTeam(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("SoccerTeam [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
