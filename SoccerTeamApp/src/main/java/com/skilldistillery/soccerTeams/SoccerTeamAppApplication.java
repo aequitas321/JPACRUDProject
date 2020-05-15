b71fca00d0515e5a2054c7a2f638f810368aa240
Shane Duncan <aequitas321@icloud.com>
2020-05-15 19:37:49 -0400

    Started spring boot app setup

---
+++ SoccerTeamApp/src/main/java/com/skilldistillery/soccerTeams/SoccerTeamAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.soccerTeams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerTeamAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerTeamAppApplication.class, args);
+	}
+
+}
